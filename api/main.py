86e0f0ddac9fe0a7c19714e953966178819658e9
dumbled0re <s.r19990706@gmail.com>
2022-04-30 02:03:22 +0900

    refactor: レスポンスボディのモデル定義をschemas.pyに移行

--- api/main.py
+++ api/main.py
@@ -1,30 +1,5 @@
-import datetime
 from fastapi import FastAPI
-from pydantic import BaseModel, Field
-
-
-# 会議室予約画面
-class Booking(BaseModel):
-    booking_id: int
-    user_id: int
-    room_id: int
-    booked_num: int
-    start_datetime: datetime.datetime
-    end_datetime: datetime.datetime
-
-
-# ユーザー登録画面
-class User(BaseModel):
-    user_id: int
-    user_name: str = Field(max_length=12)
-
-
-# 会議室登録画面
-class Room(BaseModel):
-    room_id: int
-    room_name: str = Field(max_length=12)
-    capacity: int
-
+from sql_app.schemas import Booking, User, Room
 
 app = FastAPI()
 
---
+++ api/sql_app/schemas.py
@@ -0,0 +1,34 @@
+import datetime
+from pydantic import BaseModel, Field
+
+# 会議室予約画面
+class Booking(BaseModel):
+    booking_id: int
+    user_id: int
+    room_id: int
+    booked_num: int
+    start_datetime: datetime.datetime
+    end_datetime: datetime.datetime
+
+    # NOTE: 基本的にはディクショナリでデータが入ってくるが、sqlalchemyなどのormのデータ構造が入ってきてもデータを読めるようにする
+    class Config:
+        orm_mode = True
+
+
+# ユーザー登録画面
+class User(BaseModel):
+    user_id: int
+    user_name: str = Field(max_length=12)
+
+    class Config:
+        orm_mode = True
+
+
+# 会議室登録画面
+class Room(BaseModel):
+    room_id: int
+    room_name: str = Field(max_length=12)
+    capacity: int
+
+    class Config:
+        orm_mode = True
