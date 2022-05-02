37650f85de293f6f178134f4b81b33966542e563
dumbled0re <s.r19990706@gmail.com>
2022-05-03 01:32:01 +0900

    feat: ミーティングAPI実装

--- api/sql_app/database.py
+++ api/sql_app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-
+# NOTE: データベース作成位置
 SQLALCHMY_DATABASE_URL = "sqlite:///./sql_app.db"
 
 engine = create_engine(
--- api/sql_app/schemas.py
+++ api/sql_app/schemas.py
@@ -2,13 +2,16 @@ import datetime
 from pydantic import BaseModel, Field
 
 # 会議室予約画面
-class Booking(BaseModel):
-    booking_id: int
+class BookingCreate(BaseModel):
     user_id: int
     room_id: int
     booked_num: int
     start_datetime: datetime.datetime
     end_datetime: datetime.datetime
+
+
+class Booking(BookingCreate):
+    booking_id: int
 
     # NOTE: 基本的にはディクショナリでデータが入ってくるが、sqlalchemyなどのormのデータ構造が入ってきてもデータを読めるようにする
     class Config:
@@ -16,19 +19,25 @@ class Booking(BaseModel):
 
 
 # ユーザー登録画面
-class User(BaseModel):
-    user_id: int
+class UserCreate(BaseModel):
     user_name: str = Field(max_length=12)
+
+
+class User(UserCreate):
+    user_id: int
 
     class Config:
         orm_mode = True
 
 
 # 会議室登録画面
-class Room(BaseModel):
-    room_id: int
+class RoomCreate(BaseModel):
     room_name: str = Field(max_length=12)
     capacity: int
+
+
+class Room(RoomCreate):
+    room_id: int
 
     class Config:
         orm_mode = True
